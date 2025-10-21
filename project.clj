6eb5d0f445b483b35d8ac60d12ca4b7495c32bd9
Jacob Emcken <jacob.emcken@ufst.dk>
2025-10-21 14:23:01 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "A generic HTTP service that converts HTML to PDF."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [com.itextpdf/html2pdf "4.0.5"]
                  [com.itextpdf/itext7-core "7.2.5" :extension "pom"]
-                 [http-kit "2.8.0"]
-                 [mount/mount "0.1.21"]
-                 [metosin/reitit-ring "0.7.2"]]
+                 [http-kit "2.8.1"]
+                 [mount/mount "0.1.23"]
+                 [metosin/reitit-ring "0.9.1"]]
   :main itext-doc-creator.core
   :repl-options {:init-ns itext-doc-creator.core})
